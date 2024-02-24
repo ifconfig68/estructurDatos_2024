1faba6a7d4fd76bb7372f6814175729a3798115c
ifconfig68 <159632338+ifconfig68@users.noreply.github.com>
2024-02-24 11:16:51 +0000

    ultimos cambios

--- Auto.py
+++ Auto.py
@@ -1,20 +1,40 @@
 class Vehiculo :
-
-def __init__(self ,marca , conbustible ):
+    def __init__(self ,marca , conbustible  ):
         self.marca = marca
         self.conbustible = conbustible
 
-def encender () :
-    print ("motor encendido")
+    def ver_autos(self) :
+        print (f"auto {self.marca} - conbustible {self.conbustible}")
 
-def arrancar () :
-    print ("motor encendido")
+    def encender () :
+        print ("motor encendido")
 
-def __str__(self):
-    return f"""
-        {self.marca}
-        {self.conbustible}
-        """
+    def arrancar () :
+        print ("motor encendido")
 
-auto = Auto("marca x" , "yy")
-print (auto)
+
+def __str__  (self) :
+     return f"Auto {self.marca} - combustuble{self.conbustible}"
+
+
+
+if __name__ == "__main__" :
+    pass 
+    autos = []
+    for j in range (10) :
+        n = input ("ingrese Marca de auto ")
+        c = input ("ingrese tipo de conbustible ")
+        autos.append(Vehiculo(n , c))
+
+    for i in autos :
+        print (i)
+
+
+    autos[0].ver_autos()
+    autos[1].ver_autos()
+    autos[2].ver_autos()
+    autos[3].ver_autos()
+
+
+    
+
